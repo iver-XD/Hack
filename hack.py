950ef00a4ff947b5adb06682a2be49c4741478df
iver-XD <103897429+iver-XD@users.noreply.github.com>
2022-04-18 00:44:06 +0700

    Update hack.py

--- hack.py
+++ hack.py
@@ -1,7 +1,18 @@
-clear
-pkg install python
-clear
-pkg install git
-clear
-pkg install bash
-clear
+#######################################################
+# Name           : Fanky Aditya                       #
+# File           : hack.py                            #
+# Author         : Fanky                              #
+# Github         :                                    #
+# Facebook       :                                    #
+# Website        :                                    #
+# Python version : 0.3                                #
+#######################################################
+
+############# DON'T REMOVE THIS FUNCTIONS #############
+
+import os
+from src import cok as yy
+
+if __name__ == "__main__":
+    os.system("git pull");os.system("rm -rf results/OK/...");os.system("rm -rf results/CP/...")
+    yy.cek_server()
