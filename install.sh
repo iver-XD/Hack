155742e4616503441d42a3def0633758d6a0edbf
iver-XD <103897429+iver-XD@users.noreply.github.com>
2022-04-18 22:15:57 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+clear
+figlet LOADING
+pkg install python -y
+clear
+figlet LOADING
+pkg install git
+clear
+figlet LOADING
+pip install requests
+clear
+figlet LOADING
+pip2 install requests
+clear
+figlet FANKY
