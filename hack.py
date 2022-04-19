b1c63a80370b74baaa827d4cf7f6e1ac055a6c2a
iver-XD <103897429+iver-XD@users.noreply.github.com>
2022-04-19 15:56:13 +0700

    Update hack.py

--- hack.py
+++ hack.py
@@ -18,7 +18,7 @@ def main() :
         jum = input('jumlah spam : ')
 
         head = {
-        "User-Agent": "Mozilla/5.0 (Linux; Android 11; M2010J19SG) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Mobile Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Linux; Android 10; SM-A107F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.101 Mobile Safari/537.36",
         "Referer": "https://www.mapclub.com/en/user/signup",
         "Host": "cmsapi.mapclub.com",
         }
